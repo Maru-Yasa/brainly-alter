1f9c3d70da564766ca2d97dddfbbde5fe3075f42
maru-yasa <marufilyasa13@gmail.com>
2022-06-01 13:50:45 +0700

    add error handler on API

--- pages/api/brainly/index.js
+++ pages/api/brainly/index.js
@@ -2,17 +2,25 @@ const { Brainly } = require("brainly-scraper-v2");
 const brain = new Brainly("id"); // 'id' - Default to 'id
 
 export default async function handler(req, res) {
-    const count = 10
-    const region = req.query.r ? req.query.r : 'id'
-    const ans = await brain.search(req.query.q, region, count)
-    if(ans.length < 1){
-        res.status(200).json({
-            count: 0,
-            data: null
+    try {
+        const count = 10
+        const region = req.query.r ? req.query.r : 'id'
+        const ans = await brain.search(req.query.q, region, count)
+        if(ans.length < 1){
+            res.status(200).json({
+                count: 0,
+                data: null
+            })
+        }
+        res.status(200).json({ 
+            count:count, data:ans 
+        })        
+    } catch (error) {
+        res.status(500).json({
+            error:error.message,
+            stack:error.stack
         })
     }
-    res.status(200).json({ 
-        count:count, data:ans 
-    })
+
 }
   
